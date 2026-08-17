a8c4ef8178b86f045ddfe96af3faef6ca19a1982
valdelisiam-a11y <valdelisiam@gmail.com>
2026-08-17 15:44:24 -0300

    feat: inicializa frontend com React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <h1>Projeto Final — +PraTi</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
