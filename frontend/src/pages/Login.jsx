159a5435236fe53ef7fe3d54b099ebc5a9f68e43
Camila Regner <milaregner@hotmail.com>
2026-08-20 21:59:50 -0300

    adiciona estilos iniciais da tela de login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,44 @@
+import './Login.css'
+
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+        <main className="login">
+
+        <section className="login-image"> {/* Lado esquerdo da tela */}
+            <div className="image-placeholder">Imagem</div> {/* Espaço reservado para a imagem */}
+        </section>
+
+        <section className="login-form"> {/* Lado direito da tela */}
+            <div className="logo">DocFlow</div>
+
+            <h1>Organize seus trabalhos de forma simples.</h1>
+
+            <form> {/* Formulário de login */}
+                <div className="form-group">
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" id="email" placeholder="voce@exemplo.com.br"                       
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" id="password" placeholder="Sua senha" 
+                    />
+                </div>
+
+                <button type="submit">Entrar</button> {/* Botão de envio */}
+
+                {/* Links de navegação */}
+                {/*
+                Para posterior implementação de outras páginas, rotas ou outra solução
+               */}
+                <a href="#">Esqueci minha senha</a>
+
+                <a href="#">Criar minha conta</a>
+            </form>
+        </section>
+    </main>
+    );
 }
 
-export default Login
+export default Login;
